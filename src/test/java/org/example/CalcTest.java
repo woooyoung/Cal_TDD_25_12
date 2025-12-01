cea45e74ec3ed67a49e9fead3b9a0473bcbc37ff
woooyoung <axdsw121@gmail.com>
2025-12-01 12:39:34 +0900

    2 - 1

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -36,4 +36,24 @@ public class CalcTest {
     public void test5() {
         assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
     }
+
+    @Test
+    @DisplayName("2 - 1 = 1")
+    public void test6() {
+        assertThat(Calc.run("2 - 1")).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("50 - 30 = 20")
+    public void test7() {
+        assertThat(Calc.run("50 - 30")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("7 - 4 = 3")
+    public void test8() {
+        assertThat(Calc.run("7 - 4")).isEqualTo(3);
+    }
+
+
 }
