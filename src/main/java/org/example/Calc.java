02c73711d952ad200c8bc725b960fceeec885c95
woooyoung <axdsw121@gmail.com>
2025-12-02 15:43:17 +0900

    바깥쪽의 소괄호 제거

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -7,6 +7,10 @@ public class Calc {
 
     public static int run(String exp) {
 
+        // 괄호 제거
+        exp = stripOuterBrackets(exp);
+
+        // 그냥 숫자만 들어올 경우 바로 리턴
         if (!exp.contains(" ")) {
             return Integer.parseInt(exp);
         }
@@ -52,6 +56,14 @@ public class Calc {
 
 
         throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
+    }
+
+    private static String stripOuterBrackets(String exp) {
+        if(exp.charAt(0) == '(' && exp.charAt(exp.length() - 1) == ')') {
+            exp = exp.substring(1, exp.length() - 1);
+        }
+
+        return exp;
     }
 
 
